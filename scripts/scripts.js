eb78e0159ed8f16f7bfff95c823cc3fe21b4351b
Andirios <75648378+Andirios@users.noreply.github.com>
2020-12-27 15:54:24 -0800

    Added player

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -25,6 +25,21 @@ Obstaclerun = {
     }
     return player; 
   },
+  //createPlayer: function (){
+  // let player = document.createElement("div")
+  // fishes.classname= "player"
+  //let playerArray = [
+  // {object: 'player', image: "https://img.favpng.com/2/6/13/image-fish-portable-network-graphics-download-png-favpng-ebhYVWTEHGsTG2iafehsBLJP8.jpg"},
+  // ]
+  
+  // for (let i = 0; i < this.player.length; i + = 1){
+  // var playerImage = this.player[i]
+  // var obj = getRandomItem(playerArray) 
+  // this.playerArray[i].push(this.player)
+  // player[i].innerHTML = obj.name + '<img srcs="" + ' > '
+  // document.getElementById('animation_container').appendChild(fishImage);
+  // }
+ // }, 
 
   createObstacle: function () {
 
@@ -35,6 +50,12 @@ Obstaclerun = {
 
 
   },
+  render:function() {
+    for(let i = 0; i <1; i++){
+      this.player[i].element.style.top = this.people[i].y_pos + "px"
+      this.player[i].element.style.left = this.people[i].x_pos + "px"
+  }
+},
 }
 
 Obstaclerun.init();
