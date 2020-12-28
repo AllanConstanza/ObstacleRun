34cd458144b0bf4fd37388800ccbea3d67673553
Andirios <75648378+Andirios@users.noreply.github.com>
2020-12-28 11:34:49 -0800

    Changed js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,44 +11,7 @@ Obstaclerun = {
     this.startSimulation();
 
     this.renderObstacles();
-
-
-   // this.render();
-   //this.startGame();
   },
-
- /**  createPlayer: function () {
-    let playerdiv = document.createElement("div"); 
-    playerdiv.className = "player"
-    this.container.append(playerdiv);
-    let player = {
-      x_pos: Math.random()* 600,
-      y_pos: Math.random()* 500, 
-      x_velocity: Math.random()* 1,
-      y_velocity: Math.random()* 1, 
-      radius:5 , 
-      element: playerdiv
-    }
-    return player; 
-  }, **/
-
-
-  //createPlayer: function (){
-  // let player = document.createElement("div")
-  // fishes.classname= "player"
-  //let playerArray = [
-  // {object: 'player', image: "https://img.favpng.com/2/6/13/image-fish-portable-network-graphics-download-png-favpng-ebhYVWTEHGsTG2iafehsBLJP8.jpg"},
-  // ]
-  
-  // for (let i = 0; i < this.player.length; i + = 1){
-  // var playerImage = this.player[i]
-  // var obj = getRandomItem(playerArray) 
-  // this.playerArray[i].push(this.player)
-  // player[i].innerHTML = obj.name + '<img srcs="" + ' > '
-  // document.getElementById('animation_container').appendChild(fishImage);
-  // }
- // }, 
-
   createObstacle: function () {
     let obstaclediv = document.createElement("div");
     obstaclediv.className = "obstacle";
