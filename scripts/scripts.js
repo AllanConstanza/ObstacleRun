ba78c948977a6d6f5d2ff73fd7401fbc55720541
AllanConstanza <allanconstanza2003@gmail.com>
2021-01-02 22:51:52 -0800

    Made it so start button moves obstacles

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,16 +2,20 @@ Obstaclerun = {
   container: document.getElementById("obstaclerun_container"),
   obstacles: [],
   playerImages: [],
-  startbutton: document.getElementById("start"), 
+  startbutton: document.getElementById("start"),
+  simulation: undefined,
+
 
   init: function () {
     for (let i = 0; i < 6; i++) {
       this.obstacles.push(this.createObstacle());
     }
+    Obstaclerun.renderObstacles();
+
     this.startbutton.onclick = function() {
-      Obstaclerun.animateObstacles ();
+      Obstaclerun.startGame();
     }
-    this.renderObstacles();
+
   },
   createObstacle: function () {
     let obstaclediv = document.createElement("div");
@@ -19,19 +23,21 @@ Obstaclerun = {
     this.container.append(obstaclediv);
     let obstacle = {
       x_pos: Math.random() * 400,
-      y_pos:  Math.random() * 400 ,
+      y_pos: Math.random() * 400,
       x_velocity: Math.random() * 10 - 1,
-      y_velocity:Math.random() * 10 - 1,
+      y_velocity: Math.random() * 10 - 1,
       radius: 5,
       element: obstaclediv
     }
     return obstacle;
   },
 
- //will have to change this later
-     startGame: function () {
+
+  startGame: function () {
+    this.simulation = window.setInterval(this.animateObstacles.bind(Obstaclerun), 30);
+  },
+
   
-  }, 
 
   animateObstacles: function () {
     this.bounceObstacles();
@@ -64,8 +70,8 @@ Obstaclerun = {
   moveObstacles: function () {
 
     for (i = 0; i < this.obstacles.length; i++) {
-        this.obstacles[i].x_pos = this.obstacles[i].x_pos + this.obstacles[i].x_velocity;
-        this.obstacles[i].y_pos = this.obstacles[i].y_pos + this.obstacles[i].y_velocity;
+      this.obstacles[i].x_pos = this.obstacles[i].x_pos + this.obstacles[i].x_velocity;
+      this.obstacles[i].y_pos = this.obstacles[i].y_pos + this.obstacles[i].y_velocity;
     }
   },
   renderObstacles: function () {
@@ -76,20 +82,14 @@ Obstaclerun = {
 
   },
 
-  //will need a start button here 
-  startGame: function (){
 
-  },
-  endGame: function() {
 
-  },
-  
- /**  render:function() {
-    for(let i = 0; i <1; i++){
-      this.player[i].element.style.top = this.people[i].y_pos + "px"
-      this.player[i].element.style.left = this.people[i].x_pos + "px"
-  } 
-},**/
+  /**  render:function() {
+     for(let i = 0; i <1; i++){
+       this.player[i].element.style.top = this.people[i].y_pos + "px"
+       this.player[i].element.style.left = this.people[i].x_pos + "px"
+   } 
+ },**/
 }
 
 Obstaclerun.init();
